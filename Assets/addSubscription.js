9bc4f403de4b164a343541b9e7bc6bfef4894ff8
Jon O'Connell <jjoc0205@gmail.com>
2023-10-03 08:57:53 -0400

    taking input from sidebar

--- Assets/addSubscription.js
+++ Assets/addSubscription.js
@@ -1,14 +1,30 @@
 function addSub(event) {
-    alert("clicked");
+    //alert("clicked");
+    //Getting values
+    let subName = document.getElementById('expenseName').value;
+    let subCost = document.getElementById('expenseCost').value;
+    if (subName == "" || subCost == "") {
+      alert("Please fill out all fields");
+    }
+    else{
+    //Outer Div
     let newSub = document.createElement('div');
-    let subName = document.createElement('h1');
-    let subPrice = document.createElement('h2');
-    newSub.innerHTML = document.getElementById('subNameInput').value;
-    subPrice.innerHTML = document.getElementById('subPriceInput').value;
-    document.getElementById('container').appendChild(newSub);
-    newSub.appendChild(subName);
-    newSub.appendChild(subPrice);
-    
+    newSub.className = "row";
+    newSub.id = "subName";
+    //Inner Div
+    let newSubInner = document.createElement('h1');
+    newSubInner.className = "name";
+    newSubInner.innerHTML = subName;
+    let newSubInner2 = document.createElement('h4');
+    newSubInner2.className = "cost";
+    newSubInner2.innerHTML = subCost;
+
+
+    //Appending
+    newSub.appendChild(newSubInner);
+    newSub.appendChild(newSubInner2);
+    document.getElementById('subList').appendChild(newSub);
     event.preventDefault();
   }
+}
   document.addEventListener('submit', addSub);
--- Assets/sidebar.js
+++ Assets/sidebar.js
@@ -15,30 +15,30 @@ class Sidebar extends HTMLElement {
         <div class="row">Input Info</div>
         <form action="/somethinggoeshere">
             <label for="name">Expense name:</label>
-            <input type="text" id="name" name="name">
+            <input type="text" id="expenseName" name="name">
             <label for="lname">Expense Cost:</label>
-            <input type="text" id="cost" name="cost">
+            <input type="text" id="expenseCost" name="cost">
             <input type="submit" value="Submit">
         </form> 
         <form action="/somethinggoeshere">
             <label for="name">Asset name:</label>
-            <input type="text" id="name" name="name">
+            <input type="text" id="assetName" name="name">
             <label for="lname">Asset Value:</label>
-            <input type="text" id="cost" name="cost">
+            <input type="text" id="assetCost" name="cost">
             <input type="submit" value="Submit">
         </form> 
         <form action="/somethinggoeshere">
             <label for="name">Monthly Income:</label>
-            <input type="text" id="name" name="name">
+            <input type="text" id="incomeName" name="name">
             <input type="submit" value="Submit">
         </form> 
         <form action="/somethinggoeshere">
             <label for="name">Goal:</label>
-            <input type="text" id="name" name="name">
+            <input type="text" id="goalName" name="name">
             <label for="lname">Goal Cost:</label>
-            <input type="text" id="cost" name="cost">
+            <input type="text" id="goalCost" name="cost">
             <label for="lname">Goal Date:</label>
-            <input type="text" id="date" name="date">
+            <input type="text" id="goalDate" name="date">
             <input type="submit" value="Submit">
         </form> 
       </div>
