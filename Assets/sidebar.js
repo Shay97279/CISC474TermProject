4ca2271a6cf8450e820580ac1840badd087b828e
patelsap <patelsap@udel.edu>
2023-11-16 08:33:47 -0500

    added breaks and text styling to js string

--- Assets/sidebar.js
+++ Assets/sidebar.js
@@ -8,30 +8,30 @@ class Sidebar extends HTMLElement {
       this.innerHTML = `<div class="sidebar">
       <div class="row">Input Info</div>
       <form id="expenses">
-        <label for="expenseName">Expense name:</label>
+        <label for="expenseName" style = "text-align:center;">Expense name:</label>
         <input type="text" id="expenseName" name="name">
-        <label for="expenseCost">Expense Cost:</label>
+        <label for="expenseCost" style = "text-align:center;">Expense Cost:</label>
         <input type="text" id="expenseCost" name="cost">
-        <input type="submit" value="submit">
+        <input type="submit" value="submit"><br>
       </form>
       <form id="assets">
-        <label for="assetName">Asset name:</label>
+        <label for="assetName" style = "text-align:center;">Asset name:</label>
         <input type="text" id="assetName" name="name">
-        <label for="assetValue">Asset Value:</label>
+        <label for="assetValue" style = "text-align:center;">Asset Value:</label>
         <input type="text" id="assetValue" name="cost">
-        <input type="submit" value="Submit">
+        <input type="submit" value="Submit"><br>
       </form>
       <form id="income">
-        <label for="monthlyIncome">Monthly Income:</label>
+        <label for="monthlyIncome" style = "text-align:center;">Monthly Income:</label>
         <input type="text" id="monthlyIncome" name="name">
-        <input type="submit" value="Submit">
+        <input type="submit" value="Submit"><br>
       </form>
       <form id="goals">
-        <label for="goalName">Goal:</label>
+        <label for="goalName" style = "text-align:center;">Goal:</label>
         <input type="text" id="goalName" name="name">
-        <label for="goalCost">Goal Cost:</label>
+        <label for="goalCost" style = "text-align:center;">Goal Cost:</label>
         <input type="text" id="goalCost" name="cost">
-        <label for="goalDate">Goal Date:</label>
+        <label for="goalDate" style = "text-align:center;">Goal Date:</label>
         <input type="text" id="goalDate" name="date">
         <input type="submit" value="Submit">
       </form>
