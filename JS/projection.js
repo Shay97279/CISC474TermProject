67e9f96df7f2b8eb2b6f418aa54fd126a108b84f
Kurt Hammen <khammen@udel.edu>
2023-11-27 10:55:14 -0500

    Animated graph

--- JS/projection.js
+++ JS/projection.js
@@ -5,97 +5,149 @@ document.addEventListener("DOMContentLoaded", function () {
         const monthsToAchieveGoal = parseInt(document.getElementById("monthsToAchieve").value) || 0;
         const monthlySavings = parseFloat(document.getElementById("monthlySavings").value) || 0;
 
-        // Remove the existing SVG element
-        d3.select("#projection svg").remove();
-        
-        //Create set of data points with value for each month
         const savingsData = [];
         for (let i = 0; i <= monthsToAchieveGoal; i++) {
             savingsData.push({ x: i, y: i * monthlySavings });
         }
+        // Select the existing SVG element
+        const svg = d3.select("#projection svg");
+        // Transition duration
+        const duration = 1000;
 
-        const margin = { top: 20, right: 50, bottom: 50, left: 60 }; 
-        const width = 500 - margin.left - margin.right;
-        const height = 300 - margin.top - margin.bottom;
+        // Set up scales
+        xScale.domain([0, monthsToAchieveGoal  + (monthsToAchieveGoal/5)]);
+        yScale.domain([0, targetAmount + (targetAmount/5)]);
 
-        // Create SVG element within the "projection" div
-        const svg = d3.select("#projection")
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        // Update the x-axis scale with transition
+        svg.select(".x-axis")
+            .transition()
+            .duration(duration)
+            .call(d3.axisBottom(xScale));
 
-        const xScale = d3.scaleLinear()
-            .domain([0, monthsToAchieveGoal])
-            .range([0, width]);
+        // Update the y-axis scale with transition
+        svg.select(".y-axis")
+            .transition()
+            .duration(duration)
+            .call(d3.axisLeft(yScale));
 
-        const yScale = d3.scaleLinear()
-            .domain([0, targetAmount])
-            .range([height, 0]);
-
-        const savingsLine = d3.line()
-            .x(d => xScale(d.x))
-            .y(d => yScale(d.y));
-
-
-        // Define line function(s)
-        //THIS WILL CHANGE TO BE WHATEVERY WE'RE TRYING TO PLOT
-
-        //Savings over time
-        svg.append("path")
+        // Transition for the savings progression line
+        svg.select(".line")
             .datum(savingsData)
-            .attr("class", "line")
-            .attr("d", savingsLine)
-            .attr("fill", "none")
-            .attr("stroke", "green");
+            .transition()
+            .duration(duration)
+            .attr("d", savingsLine);
 
-        //target amount to save
-        svg.append("line")
-            .attr("class", "target-line")
-            .attr("x1", 0)
+        // Transition for the horizontal target line
+        svg.select(".target-line")
+            .transition()
+            .duration(duration)
             .attr("y1", yScale(targetAmount))
-            .attr("x2", width)
-            .attr("y2", yScale(targetAmount))
-            .attr("stroke", "red")
-            .attr("stroke-dasharray", "5,5");
+            .attr("y2", yScale(targetAmount));
 
-        //target month to achieve by
-        svg.append("line")
-            .attr("class", "timeframe-line")
+        // Transition for the vertical timeframe line
+        svg.select(".timeframe-line")
+            .transition()
+            .duration(duration)
             .attr("x1", xScale(monthsToAchieveGoal))
-            .attr("y1", 0)
-            .attr("x2", xScale(monthsToAchieveGoal))
-            .attr("y2", height)
-            .attr("stroke", "blue")
-            .attr("stroke-dasharray", "5,5");
-
-        //Axes
-        //Mess with margins and x/y attributes for the axis labels to get them in the right spot
-        // Append x-axis with label
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(xScale))
-            
-        svg.append("text")
-            .attr("class", "x label")
-            .attr("x", width / 2)
-            .attr("y", height + 35)
-            .style("text-anchor", "middle")
-            .text("Months");
-
-        // Append y-axis with label
-        svg.append("g")
-            .call(d3.axisLeft(yScale))
-            
-        svg.append("text")
-            .attr("class", "axis-label")
-            .attr("transform", "rotate(-90)")
-            .attr("y", -margin.left + 20)
-            .attr("x", -height / 2)
-            .style("text-anchor", "middle")
-            .text("Savings");
+            .attr("x2", xScale(monthsToAchieveGoal));
+        
+        
     };
+
+    //Create first graph
+    const targetAmount = 1200;
+    const monthsToAchieveGoal = 12;
+    const monthlySavings = 100;
+
+    //Create set of data points with value for each month
+    const savingsData = [];
+    for (let i = 0; i <= monthsToAchieveGoal; i++) {
+        savingsData.push({ x: i, y: i * monthlySavings });
+    }
+
+    const margin = { top: 20, right: 50, bottom: 50, left: 60 }; 
+    const width = 500 - margin.left - margin.right;
+    const height = 300 - margin.top - margin.bottom;
+
+    // Create SVG element within the "projection" div
+    const svg = d3.select("#projection")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    const xScale = d3.scaleLinear()
+        .domain([0, monthsToAchieveGoal + (monthsToAchieveGoal/5)])
+        .range([0, width]);
+
+    const yScale = d3.scaleLinear()
+        .domain([0, targetAmount  + (targetAmount/5)])
+        .range([height, 0]);
+
+    const savingsLine = d3.line()
+        .x(d => xScale(d.x))
+        .y(d => yScale(d.y));
+
+
+    // Define line function(s)
+    //THIS WILL CHANGE TO BE WHATEVERY WE'RE TRYING TO PLOT
+
+    //Savings over time
+    svg.append("path")
+        .datum(savingsData)
+        .attr("class", "line")
+        .attr("d", savingsLine)
+        .attr("fill", "none")
+        .attr("stroke", "green");
+
+    //target amount to save
+    svg.append("line")
+        .attr("class", "target-line")
+        .attr("x1", 0)
+        .attr("y1", yScale(targetAmount))
+        .attr("x2", width)
+        .attr("y2", yScale(targetAmount))
+        .attr("stroke", "red")
+        .attr("stroke-dasharray", "5,5");
+
+    //target month to achieve by
+    svg.append("line")
+        .attr("class", "timeframe-line")
+        .attr("x1", xScale(monthsToAchieveGoal))
+        .attr("y1", 0)
+        .attr("x2", xScale(monthsToAchieveGoal))
+        .attr("y2", height)
+        .attr("stroke", "blue")
+        .attr("stroke-dasharray", "5,5");
+
+    //Axes
+    //Mess with margins and x/y attributes for the axis labels to get them in the right spot
+    // Append x-axis with label
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale))
+        .attr("class", "x-axis")
+        
+    svg.append("text")
+        .attr("class", "x label")
+        .attr("x", width / 2)
+        .attr("y", height + 35)
+        .style("text-anchor", "middle")
+        .text("Months");
+
+    // Append y-axis with label
+    svg.append("g")
+        .call(d3.axisLeft(yScale))
+        .attr("class", "y-axis");
+        
+    svg.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -margin.left + 20)
+        .attr("x", -height / 2)
+        .style("text-anchor", "middle")
+        .text("Savings");
 
     updateGraph();
 });
