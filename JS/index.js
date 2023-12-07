a52edcd669b31f86789d0f23da06c96310339655
Daniel Yang <daniely@udel.edu>
2023-12-06 23:53:33 -0500

    index.js

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,21 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const hbs = require("hbs")
+
+const templatePath = path.join(__dirname, '../templates')
+
+app.use(express.json())
+app.set("view engine", "hbs")
+app.set("views", templatePath)
+
+app.get("/",(req, res) => {
+    res.render("login")
+})
+
+app.get("/signup",(req, res) => {
+    res.render("signup")
+})
 
 app.listen(3000, ()=>{
     console.log("port connected");
