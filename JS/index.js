40c007e3a59d178c767f7e25e3f2873cfcc85ec9
Daniel Yang <daniely@udel.edu>
2023-12-11 15:05:46 -0500

    login & signup

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-
-
+const collection = require("./mongodb")
 
 app.use(express.json())
 app.set("view engine", "hbs")
 app.set("views", templatePath)
+app.use(express.urlencoded({extended: false}))
 
 app.get("/",(req, res) => {
     res.render("login")
@@ -14,6 +14,31 @@ app.get("/",(req, res) => {
 
 app.get("/signup",(req, res) => {
     res.render("signup")
+})
+
+app.post("/signup",async(req, res) => {
+    const data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }
+    await collection.insertMany([data])
+    res.render("landing")
+})
+
+app.post("/login",async(req, res) => {
+    try{
+        const check = await collection.findOne({email: req.body.email})
+        if(check.password === req.body.password){
+            res.render("landing")
+        }
+        else{
+            res.send("Wrong password")
+        }
+    }
+    catch{
+        res.send("Wrong details")
+    }
 })
 
 app.listen(3000, ()=>{
