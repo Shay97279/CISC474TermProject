14369cdcad1dc2e640f7141d430f5e20f6d64509
Kurt Hammen <khammen@udel.edu>
2023-12-13 20:12:01 -0500

    subscriptions page integrated

--- JS/getfromDB.js
+++ JS/getfromDB.js
@@ -8,6 +8,11 @@ function extractExpenses(userdata, user){
     console.log(newuser.expenses);
     return newuser.expenses;
 }
+function extractSubscriptions(userdata, user){
+    const newuser = userdata.find(entry => entry.firstName === user);
+    const subs = newuser.expenses.filter(entry => (entry.category === 'subscriptions' || entry.category === 'subscription'))
+    return subs;
+}
 
 
 $('#getbutton').click(function(){
@@ -19,6 +24,7 @@ $('#getbutton').click(function(){
             localStorage.setItem('users', JSON.stringify(result));
             localStorage.setItem('assets', JSON.stringify(extractAssets(result, "Jonathon")));
             localStorage.setItem('expenses', JSON.stringify(extractExpenses(result, "Jonathon")));
+            localStorage.setItem('subscriptions', JSON.stringify(extractSubscriptions(result, "Jonathon")));
         }
     });
 })
--- server.js
+++ server.js
@@ -212,14 +212,13 @@ app.get('/allUsers', async (req, res) => {
 	}
 });
 app.get('/user', async (req, res) => {
-	try {
-		const users = await User.findById(req.query.id);
-		console.log(users);
-		res.send(users);
-	}
-	catch (err) {
-		console.log("error" + err);
-	}
+    try {
+        const users = await User.find();
+        res.send(users);
+    }
+    catch (err) {
+        console.log("error" + err);
+    }
 });
 
 app.listen(port, () => {
